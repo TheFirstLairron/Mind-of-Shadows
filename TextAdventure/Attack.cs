5ec41cf5cf1bfa8b372cb42ab3ed5f484701f7f7
TheFirstLairron <zrwall@hotmail.com>
2016-04-07 15:26:36 -0700

    Trying again to get the ignore working

---
+++ TextAdventure/Attack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    public class Attack
+    {
+        public string name { get; set; }
+        public string writtenIdentifier { get; set; }
+        public string amountOfDamage { get; set; }
+    }
+}
